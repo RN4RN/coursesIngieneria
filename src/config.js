eb84ee6bcd7732853b1418d4dde25f986ac9b5c1
RN4RN <grijalvauve@gmail.com>
2025-08-06 16:19:04 -0500

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// Este archivo contendrá constantes globales para la aplicación.
-
-// Datos para la compra a través de WhatsApp
-export const WHATSAPP_NUMBER = '929985151'; // Tu número de WhatsApp
-export const OWNER_NAME = 'Ing. Ronald Rios Noriega'; // Tu nombre
